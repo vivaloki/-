71fea67defafcb02c452ac0f475001aa04891102
vivaloki <jenny2960@hotmail.com>
2018-03-07 22:23:25 +0800

    first commit

---
+++ pages/welcome/welcome.js
@@ -0,0 +1,14 @@
+Page({
+  onTap: function (event) {
+    // wx.navigateTo({
+    //     url:"../posts/post"
+    // });
+
+
+    //swichTab定义到的下一页是有tab的
+    wx.switchTab({
+      url: "../posts/post"
+    });
+
+  }
+})
